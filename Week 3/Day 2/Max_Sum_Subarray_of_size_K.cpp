a8e44b4a14f9075a22701b42d5ab68d642dac4ff
riazsorkar <riazsorkar709@gmail.com>
2024-11-13 22:28:24 +0600

    Week 3 day 2

---
+++ Week 3/Day 2/Max_Sum_Subarray_of_size_K.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+   long long maximumSumSubarray(vector<int>& a, int k) {
+      int n = a.size(), l = 0, r = 0;
+      long long ans = 0, sum = 0;
+      while (r < n) {
+         sum += a[r];
+         if (r - l + 1 == k) {
+            ans = max(ans, sum);
+            sum -= a[l];
+            l++, r++;
+         }
+         else {
+            r++;
+         }
+      }
+      return ans;
+   }
+};
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    Solution();
+    return 0;
+}
