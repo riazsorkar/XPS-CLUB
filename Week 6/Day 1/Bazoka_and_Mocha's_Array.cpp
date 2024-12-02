f4019df1ea0858dc644f1e87462eacaffb376933
riazsorkar <riazsorkar709@gmail.com>
2024-12-03 00:23:13 +0600

    Week 6 day 1

--- Week 6/Day 1/Bazoka_and_Mocha's_Array.cpp
+++ Week 6/Day 1/Bazoka_and_Mocha's_Array.cpp
@@ -1,50 +1,48 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-bool solve(vector<int> &a) {
-    int n = a.size();
-    for (int i = 0; i < n; i++) {
-        a.push_back(a[i]);
-    }
-
-    // Check if array is cylically sorted or not.
-    for (int start = 0; start < n; start++) {
-        bool is_sorted = true;
-        int last = -1e6;
-        for (int i = start; i < start + n; i++) {
-            if (a[i] < last) {
-                is_sorted = false;
-                break;
-            }
-            last = a[i];
-        }
-        if (is_sorted) {
-            return true;
-        }
-    }
-    return false;
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int t;
-    cin >> t;
-    for (int zz = 0; zz < t; zz++) {
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int x;
+    cin >> x;
+    for (int i = 0; i < x; i++) {
         int n;
         cin >> n;
         vector<int> a(n);
         for (int i = 0; i < n; i++) {
             cin >> a[i];
         }
-        if (solve(a)) {
-            cout << "Yes"
-                 << "\n";
+
+        
+        for (int i = 0; i < n; i++) {
+            a.push_back(a[i]);
+        }
+
+        bool flag = false;
+
+       
+        for (int start = 0; start < n; start++) {
+            bool sorted = true;
+            int last = -1e6;
+            for (int i = start; i < start + n; i++) {
+                if (a[i] < last) {
+                    sorted = false;
+                    break;
+                }
+                last = a[i];
+            }
+            if (sorted) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag) {
+            cout << "Yes" <<"\n";
         } else {
-            cout << "No"
-                 << "\n";
+            cout << "No" <<"\n";
         }
     }
+
     return 0;
 }
